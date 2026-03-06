6c4271b1adb07a13b52ee145ab07d5b0d273358a
edgeidr <edge.idr@gmail.com>
2026-03-06 14:07:10 +0800

    chore(web): install @nuxtjs/i18n module

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	ssr: false,
+	modules: ["@nuxtjs/i18n"],
+	i18n: {
+		locales: [{ code: "en", language: "en-US", file: "en.json" }],
+		defaultLocale: "en",
+		restructureDir: "",
+		strategy: "no_prefix",
+		langDir: "../../packages/i18n/locales",
+	},
 });
