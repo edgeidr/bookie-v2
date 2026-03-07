ed65e5f9b2c2caa7b236e4aa05eae90431f12287
edgeidr <edge.idr@gmail.com>
2026-03-07 12:11:19 +0800

    chore(web): install @nuxt/ui module

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	ssr: false,
-	modules: ["@nuxtjs/i18n"],
+	modules: ["@nuxtjs/i18n", "@nuxt/ui"],
+	css: ["~/assets/css/main.css"],
 	i18n: {
 		locales: [{ code: "en", language: "en-US", file: "en.json" }],
 		defaultLocale: "en",
